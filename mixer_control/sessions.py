42a1bf35769a11c79dc9a06afa6541bf9b249329
Justin Patrin <papercrane@reversefold.com>
2021-05-25 16:05:28 -0700

    Remove debug and commented code. Raise exceptions when a device or process is not found

--- mixer_control/sessions.py
+++ mixer_control/sessions.py
@@ -12,40 +12,6 @@ from pyWinCoreAudio import (
 )
 
 
-# class AudioSession(object):
-#     def __init__(self, session, lpcguid):
-#         self._session = session
-#         self._lpcguid = lpcguid
-
-#     def _get_volume(self):
-#         if hasattr(self._session, "SimpleAudioVolume"):
-#             return self._clean_session_volume(
-#                 self._session.SimpleAudioVolume.GetMasterVolume()
-#             )
-
-#         return self._clean_session_volume(self._session.GetMasterVolumeLevelScalar())
-
-#     def _set_volume(self, value):
-#         if hasattr(self._session, "SimpleAudioVolume"):
-#             self._session.SimpleAudioVolume.SetMasterVolume(value, self._lpcguid)
-#         else:
-#             self._session.SetMasterVolumeLevelScalar(value, self._lpcguid)
-
-#     volume = property(_get_volume, _set_volume)
-
-#     # Clamp to 1.2 digits
-#     def _clean_session_volume(self, value):
-#         return math.floor(value * 100) / 100.0
-
-#     def _get_mute(self):
-#         return self._session.GetMute(self._lpcguid) == 1
-
-#     def _set_mute(self, value):
-#         self._session.SetMute(1 if value else 0, self._lpcguid)
-
-#     mute = property(_get_mute, _set_mute)
-
-
 class SessionController(object):
     __instance = None
 
@@ -86,24 +52,6 @@ class SessionController(object):
         self.sessions = list(audio_session_manager)
         gc.collect()
 
-        # active_device = pycaw.AudioUtilities.GetSpeakers()
-        # active_device_interface = active_device.Activate(
-        #     pycaw.IAudioEndpointVolume._iid_, comtypes.CLSCTX_ALL, None
-        # )
-        # self.master_session = AudioSession(
-        #     comtypes.cast(
-        #         active_device_interface, ctypes.POINTER(pycaw.IAudioEndpointVolume)
-        #     )
-        # )
-
-        # self._sessions = [
-        #     AudioSession(s) for s in pycaw.AudioUtilities.GetAllSessions()
-        # ]
-
-        # self._devices = [
-        #     d for d in pycaw.AudioUtilities.GetAllDevices()
-        # ]
-
     def maybe_refresh(self):
         if self._last_session_refresh is None or datetime.datetime.now() - self._last_session_refresh > datetime.timedelta(minutes=5):
             self.refresh()
@@ -117,18 +65,14 @@ class SessionController(object):
                 return process
             if f"\\{name}%b" in process.id:
                 return process
-        print(self.sessions)
-        import ipdb; ipdb.set_trace()
-        return None
+        raise Exception(f"No process named {name} found")
 
     def get_device(self, name):
         self.maybe_refresh()
         for device, endpoint in self.device_endpoints:
             if endpoint.name == name:
                 return endpoint
-        print(self.device_endpoints)
-        import ipdb; ipdb.set_trace()
-        return None
+        raise Exception(f"No device named {name} found")
 
     @classmethod
     def instance(cls):
